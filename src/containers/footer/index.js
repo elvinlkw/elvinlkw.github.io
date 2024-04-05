e03b707960980d7d5be713f07610b273a1a07f93
Elvin Li Kam Wa <elvinlkw@hotmail.com>
2024-04-04 23:28:28 -0400

    removed unused import

--- src/containers/footer/index.js
+++ src/containers/footer/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StyledFooter } from './styles';
-import useScroll from 'src/hooks/useScroll';
 
 const Footer = () => {
   return <StyledFooter>&copy; 2024</StyledFooter>;
